ab7db232920b16a8c36975fa7cda95a39f50cf81
jbrown400 <justin.brown400@gmail.com>
2018-12-02 12:16:51 -0600

    Added a bunch of other tools.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,43 @@
 
 # Run this file to install all tools, plugins, etc.
 
+echo "Before we start, has this computer been connected to Justin's GitHub?"
+### get user input
+
+
+# ensure script is run from home directory
+cd
+
 echo "Installing brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Installing tmux"
+brew install tmux
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 echo "Installing vim"
 brew install vim
+
+echo "Installing python"
+brew install python
+
+echo "Installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Installing powerline fonts"
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+echo "Installing Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+
 
 
 
